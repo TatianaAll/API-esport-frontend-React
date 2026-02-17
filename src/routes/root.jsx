d36242ccb9d293ca1598db7f588050f5086b7b42
TatianaAll <tatiana.allery@orange.fr>
2026-02-17 10:16:49 +0100

    FEAT: add router and architecture

---
+++ src/routes/root.jsx
@@ -0,0 +1,18 @@
+/* router file for guiding with URL */
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Error from "../pages/Error";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {path: "/", element: <Home />}
+    ],
+  },
+]);
+
+export default router;
