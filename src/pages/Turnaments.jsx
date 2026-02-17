4c567de6825239066e62a3d8bc45ba285f0139aa
TatianaAll <tatiana.allery@orange.fr>
2026-02-17 15:48:53 +0100

    FEAT: strcuture of turnaments' page

--- src/pages/Turnaments.jsx
+++ src/pages/Turnaments.jsx
@@ -3,17 +3,92 @@ import Card from "../components/Card";
 
 function Turnaments() {
   return (
-    <section>
-      <div className="w-[50%] mt-4">
-        <Card
-          infoCTA="test"
-          name="Test"
-          info="coucou"
-          photo="/images/stardew-valley.jpg"
-          linkToCTA="#"
-        />
-      </div>
-    </section>
+    <>
+      <section>
+        <div className="bg-latte w-[40%] mx-auto p-8 rounded-2xl mt-5">
+          <h3 className="font-Mitr text-center text-2xl text-chocolate mb-2">
+            Last turnament
+          </h3>
+          <Card
+            infoCTA="Voir plus"
+            name="Test"
+            info="coucou"
+            photo="/images/stardew-valley.jpg"
+            linkToCTA="#"
+          />
+        </div>
+      </section>
+      <section className="my-5 py-5">
+        <h3 className="font-Mitr text-center text-2xl text-latte mb-2">
+          Upcoming
+        </h3>
+        <div className="flex gap-4 justify-center">
+          <div className="w-[30%] mt-4">
+            <Card
+              infoCTA="test"
+              name="Test"
+              info="coucou"
+              photo="/images/stardew-valley.jpg"
+              linkToCTA="#"
+            />
+          </div>
+          <div className="w-[30%] mt-4">
+            <Card
+              infoCTA="test"
+              name="Test"
+              info="coucou"
+              photo="/images/stardew-valley.jpg"
+              linkToCTA="#"
+            />
+          </div>
+          <div className="w-[30%] mt-4">
+            <Card
+              infoCTA="test"
+              name="Test"
+              info="coucou"
+              photo="/images/stardew-valley.jpg"
+              linkToCTA="#"
+            />
+          </div>
+        </div>
+        <CTA text= "See all" buttonWidth="20%" linkTo="#" />
+      </section>
+      <section className="my-5 py-5">
+        <h3 className="font-Mitr text-center text-2xl text-latte mb-2">
+          Passed
+        </h3>
+        <div className="flex gap-4 justify-center">
+          <div className="w-[30%] mt-4">
+            <Card
+              infoCTA="test"
+              name="Test"
+              info="coucou"
+              photo="/images/stardew-valley.jpg"
+              linkToCTA="#"
+            />
+          </div>
+          <div className="w-[30%] mt-4">
+            <Card
+              infoCTA="test"
+              name="Test"
+              info="coucou"
+              photo="/images/stardew-valley.jpg"
+              linkToCTA="#"
+            />
+          </div>
+          <div className="w-[30%] mt-4">
+            <Card
+              infoCTA="test"
+              name="Test"
+              info="coucou"
+              photo="/images/stardew-valley.jpg"
+              linkToCTA="#"
+            />
+          </div>
+        </div>
+        <CTA text= "See all" buttonWidth="20%" linkTo="#" />
+      </section>
+    </>
   );
 }
 
