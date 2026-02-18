725daaabe981782cb250b5a9f70ab51f1c9696d4
TatianaAll <tatiana.allery@orange.fr>
2026-02-18 13:58:13 +0100

    FEAT: create login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,55 @@
+import { Link } from "react-router-dom";
+
 function Login() {
   return (
     <section className="bg-[url('/images/tunicbg.jpg')] bg-cover bg-center py-9 min-h-[90vh]">
-      <div className="bg-frappe rounded-2xl w-[80%] mx-auto p-4">
+      <div className="bg-frappe rounded-2xl w-[60%] mx-auto p-4">
         <div className="flex justify-center items-center gap-4 mb-4">
           <div className="w-[2%]">
-            <img
-              src="/images/lock.png"
-              alt="logo start"
-              className="w-full"
-            />
+            <img src="/images/lock.png" alt="logo start" className="w-full" />
           </div>
           <h1 className="text-chocolate text-3xl text-center my-3">Login</h1>
         </div>
         <div>
-          <input type="text" />
-          <input type="text" />
+          <div className="flex flex-col">
+            <label htmlFor="email" className="m-2 text-chocolate">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="bg-light rounded-xl p-4 m-2 text-chocolate"
+              required />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="m-2 text-chocolate">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="bg-light rounded-xl p-4 m-2 text-chocolate"
+              required />
+            <Link
+              to="#"
+              className="text-end text-matcha hover:text-chocolate duration-300 ease-in mx-2" >
+              Fogot password ?
+            </Link>
+          </div>
+          <button
+            className="text-light bg-matcha mx-auto rounded-2xl p-2 shadow-md w-28 block hover:bg-chocolate hover:text-latte duration-300 ease-in"
+            formAction="submit">
+            Login
+          </button>
         </div>
+        <p>
+          Not registred yet ? 
+          <Link to="/register" className="text-matcha hover:text-chocolate duration-300 ease-in ms-1">
+            Create an account
+          </Link>
+        </p>
       </div>
     </section>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,71 @@
+import { Link } from "react-router-dom";
+
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <section className="bg-[url('/images/tunicbg.jpg')] bg-cover bg-center py-9 min-h-[90vh]">
+      <div className="bg-matcha rounded-2xl w-[60%] mx-auto p-4">
+        <div className="flex justify-center items-center gap-4 mb-4">
+          <div className="w-[2%]">
+            <img src="/images/lock.png" alt="logo start" className="w-full" />
+          </div>
+          <h1 className="text-frappe text-3xl text-center my-3">Create an account</h1>
+        </div>
+        <div>
+          <div className="flex flex-col">
+            <label htmlFor="email" className="m-2 text-frappe">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="bg-light rounded-xl p-4 m-2 text-chocolate"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="m-2 text-frappe">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="bg-light rounded-xl p-4 m-2 text-frappe"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password2" className="m-2 text-frappe">
+              Confirm password
+            </label>
+            <input
+              type="password"
+              name="password2"
+              id="password2"
+              className="bg-light rounded-xl p-4 m-2 text-chocolate"
+              required
+            />
+          </div>
+          <button
+            className="text-light bg-chocolate mx-auto rounded-2xl p-2 shadow-md w-28 block hover:bg-frappe hover:text-chocolate duration-300 ease-in"
+            formAction="submit"
+          >
+            Register
+          </button>
+        </div>
+        <p>
+          Already have an account ?
+          <Link
+            to="/login"
+            className="text-latte hover:text-chocolate duration-300 ease-in ms-1"
+          >
+            Sign in
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
 }
 
-export default Register
+export default Register;
