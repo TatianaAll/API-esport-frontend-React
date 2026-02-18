4dcc9a6780b05685e895666c37864457c630d459
TatianaAll <tatiana.allery@orange.fr>
2026-02-18 09:47:50 +0100

    STYLE: image in Card adjustement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,8 @@ import CTA from "./CTA";
 function Card({ photo, name, info, date, infoCTA, linkToCTA }) {
   return (
     <div className="rounded-xl overflow-hidden bg-frappe">
-      <div>
-        <img src={`${photo}`} alt={`${name}`} />
+      <div className="max-w-full">
+        <img src={`${photo}`} alt={`${name}`} className="mx-auto w-full" />
       </div>
       <article className = "p-4">
         <h4 className="font-[CreamCake] text-chocolate text-3xl text-center">{name}</h4>
