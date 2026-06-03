943bcdc8be8f71815ab04a73df611b5e36469373
TatianaAll <tatiana.allery@orange.fr>
2026-06-03 13:18:32 +0200

    STYLE: change style of Game pages

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -12,42 +12,58 @@ function Games() {
       try {
         const fetchingGames = await allGames();
         setDataGames(fetchingGames);
-        setIsLoading(false); // end loading
       } catch (error) {
         console.log(error.message);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchGames();
   }, []);
 
   return (
-    <div className="relative">
+    <div className="relative min-h-screen">
+      {/* BACKGROUND */}
       <div className="absolute inset-0">
         <img
           src="/images/ac4_ss3_full.avif"
           alt="background"
-          className="object-cover w-full h-full"
+          className="object-cover w-full h-full opacity-30"
         />
       </div>
-      <section className="py-5 relative z-10">
-        <h2 className="font-Mitr text-center text-2xl text-chocolate mb-2">Games</h2>
-        {!isLoading ? (
-          dataGames.map((game) => {
-            return (
-              <div key={game._id} className="w-[80%] lg:w-[60%] mt-4 mx-auto">
-                <Card
-                  infoCTA="See details"
-                  name={game.name}
-                  info={game.publisher}
-                  photo="/images/stardew-valley.jpg"
-                  linkToCTA="#"
-                />
-              </div>
-            );
-          })
-        ) : (
-          <Spinner />
-        )}
+
+      <section className="relative z-10 py-10">
+        <h2 className="font-Mitr text-center text-3xl text-chocolate mb-8">
+          Games
+        </h2>
+
+        <div className="bg-latte w-[90%] lg:w-[75%] mx-auto p-6 rounded-2xl border border-[#e5dcd3]">
+          {/* GRID */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center">
+            {isLoading ? (
+              <Spinner />
+            ) : dataGames.length > 0 ? (
+              dataGames.map((game) => (
+                <div key={game._id} className="w-50">
+                  <Card
+                    size="md"
+                    infoCTA="See details"
+                    name={game.name}
+                    info={game.publisher}
+                    photo={
+                      game.image ? game.image : "/images/stardew-valley.jpg"
+                    }
+                    linkToCTA="#"
+                  />
+                </div>
+              ))
+            ) : (
+              <p className="col-span-full text-center text-chocolate">
+                No games available
+              </p>
+            )}
+          </div>
+        </div>
       </section>
     </div>
   );
